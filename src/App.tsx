68043724642f0a80b323491ab9a5fc0d0aff4e5d
Clee <christopherlee2012@gmail.com>
2024-06-23 21:16:08 -0400

    fixups

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import React, { useMemo, useState } from 'react';
-import SettingsDrawer from './components/SettingsDrawer';
-import { FlashCards } from './data/data';
+import React from 'react';
 import FlashCardScreen from './screens/FlashCardScreen';
 
-const flashcardData = [...FlashCards.countries.data];
-
 const App = () => {
-  const shuffledFlashcardData = useMemo(() => flashcardData.sort(() => Math.random() - 0.5), []);
-  // const shuffledFlashcardData = flashcardData;
-
-  const [flashCardData, setFlashCardData] = useState(flashcardData);
-  const [flashCardSections, setFlashCardSections] = useState(Object.keys(FlashCards));
-
   return (
     <div>
       <FlashCardScreen />
-      {/*<SettingsDrawer flashCardSections={flashCardSections} />*/}
     </div>
   );
 };
